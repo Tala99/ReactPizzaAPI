3083a94afd08f0d869051748ab287e37a8024c7d
Tala99 <hethnawi.tala@gmail.com>
2024-09-06 01:22:32 +0300

    footer edit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import style from './Footer.module.css'
 export default function Footer() {
-  return (
-    <div class="card">
-    <div class="card-header">
-      Featured
-    </div>
-    <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  )
+    return (
+        <div class="card">
+            <div class="card-header">
+                Have a nice day!
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">We would appreciate your feedback</h5>
+                <div class="form-floating">
+                    <textarea class="form-control " placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
+                    <label htmlFor="floatingTextarea2">Comments</label>
+                </div>
+                <input class="btn btn-primary mt-2" type="submit" value="Submit"/>
+            </div>
+        </div>
+    )
 }
